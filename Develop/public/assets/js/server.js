6402635d057b1b8ce80267bdfec0ef9c71e0e7c1
Rikio Campbell <rikiocamp@gmail.com>
2021-03-11 15:22:09 -0800

    added server.js with working port

---
+++ Develop/public/assets/js/server.js
@@ -0,0 +1,22 @@
+// Dependencies
+
+const express = require('express');
+const path = require('path');
+
+// Sets up the Express App
+
+const app = express();
+const PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Basic route that sends the user first to the AJAX Page
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'view.html')));
+
+app.get('/add', (req, res) => res.sendFile(path.join(__dirname, 'add.html')));
+
+// Starts the server to begin listening
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
